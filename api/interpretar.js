1a2698b7cfdc36c402ef2238b5e49c8e1c955b9d
Rodrigo <zodrigo@yahoo.com.br>
2026-04-18 15:04:09 -0300

    corrigindo export

--- api/interpretar.js
+++ api/interpretar.js
@@ -81,7 +81,7 @@ const dicionario = {
     80: ["patriarca","memória","oitenta","ancião","matriarca","história"]
   };
   
-  export default async function handler(req, res) {
+  module.exports = async function handler(req, res) {
     if (req.method !== 'POST') {
       return res.status(405).json({ erro: 'Método não permitido' });
     }
