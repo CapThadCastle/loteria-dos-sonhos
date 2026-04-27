f97a311d0a441e6126818e91ed7387ca42d11917
Rodrigo <zodrigo@yahoo.com.br>
2026-04-26 21:26:06 -0300

    verificando historico completo

--- api/verificar.js
+++ api/verificar.js
@@ -11,30 +11,55 @@ module.exports = async function handler(req, res) {
 
   try {
     const endpoint = loteria === 'megasena'
-      ? 'https://loteriascaixa-api.herokuapp.com/api/megasena/latest'
-      : 'https://loteriascaixa-api.herokuapp.com/api/lotofacil/latest';
+      ? 'https://loteriascaixa-api.herokuapp.com/api/megasena'
+      : 'https://loteriascaixa-api.herokuapp.com/api/lotofacil';
 
     const response = await fetch(endpoint);
-    const ultimo = await response.json();
+    const todos = await response.json();
 
     const numerosUser = numeros.map(Number).sort((a, b) => a - b);
+    const ultimo = todos[0];
     const numerosUltimo = (ultimo.dezenas || ultimo.listaDezenas)
       .map(Number).sort((a, b) => a - b);
 
-    const acertos = numerosUser.filter(n => numerosUltimo.includes(n)).length;
+    let combinacaoExata = false;
+    let quinaCount = 0;
+    let quadraCount = 0;
 
-    const combinacaoExata = loteria === 'megasena' ? acertos === 6 : acertos === 15;
-    const quina = loteria === 'megasena' ? acertos === 5 : null;
-    const quadra = loteria === 'megasena' ? acertos === 4 : null;
+    todos.forEach(sorteio => {
+      const dezenas = (sorteio.dezenas || sorteio.listaDezenas).map(Number);
+      const acertos = numerosUser.filter(n => dezenas.includes(n)).length;
+
+      if (loteria === 'megasena') {
+        if (acertos === 6) combinacaoExata = true;
+        if (acertos === 5) quinaCount++;
+        if (acertos === 4) quadraCount++;
+      } else {
+        if (acertos === 15) combinacaoExata = true;
+      }
+    });
+
+    const frequencia = {};
+    numerosUser.forEach(n => { frequencia[n] = 0; });
+    todos.forEach(sorteio => {
+      const dezenas = (sorteio.dezenas || sorteio.listaDezenas).map(Number);
+      dezenas.forEach(n => {
+        if (frequencia[n] !== undefined) frequencia[n]++;
+      });
+    });
+
+    const maisFrequente = Object.entries(frequencia)
+      .sort((a, b) => b[1] - a[1])[0];
 
     return res.status(200).json({
       ultimoSorteio: numerosUltimo,
       concurso: ultimo.concurso || ultimo.numero,
       combinacaoExata,
-      quina,
-      quadra,
-      acertos,
-      maisFrequente: null
+      quina: loteria === 'megasena' ? quinaCount : null,
+      quadra: loteria === 'megasena' ? quadraCount : null,
+      maisFrequente: maisFrequente
+        ? { numero: maisFrequente[0], vezes: maisFrequente[1] }
+        : null
     });
 
   } catch (e) {
